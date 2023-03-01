8815568bee2fa89894989a63158f41dcbea6faad
Swathi Paipalle <swathi.paipalle.gmail.com>
2023-02-28 18:43:30 -0800

    React Debouncing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [pin, setPin] = useState('');
+
+  useEffect(() => {
+    const debouncing = setTimeout(() => {
+      axios.get('https://api.publicapis.org/entries').then((result) => {
+        console.log('res', result);
+      });
+    }, 2000);
+    return () => {
+      clearTimeout(debouncing);
+    }
+  }, [pin])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="input" name="pin" onChange={(event) => setPin(event.target.value)} />
     </div>
   );
 }
